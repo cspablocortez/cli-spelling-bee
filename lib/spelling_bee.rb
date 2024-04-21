3f7b71d4ccf9274deab71d83cc0a08f8abdc934f
Pablo Cortez <57056367+cspablocortez@users.noreply.github.com>
2024-04-21 14:38:33 -0700

    removed console centering

--- lib/spelling_bee.rb
+++ lib/spelling_bee.rb
@@ -1,11 +1,9 @@
 require 'rainbow'
-require 'io/console'
 require_relative 'spelling_bee/puzzle'
 require_relative 'spelling_bee/game'
 
 module SpellingBee
 
   APP_NAME = 'CLI Spelling Bee'
-  TERMINAL_WIDTH = IO.console.winsize[1] rescue 80
   
 end
